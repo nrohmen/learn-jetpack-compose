9b1ada0b9a1a3b743a70e3c231619622caf9b1f2
nrohmen <nrohmen@gmail.com>
2019-10-24 12:53:17 +0700

    setup project

---
+++ app/src/main/java/com/nrohmen/learncompose/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.nrohmen.learncompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
